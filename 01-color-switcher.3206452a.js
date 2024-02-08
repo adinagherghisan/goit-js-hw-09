fe2fbf3efffcb98d9824455a8e3241bd161ddd71
adinagherghisan <141954651+adinagherghisan@users.noreply.github.com>
2024-02-08 12:50:42 +0000

    Deploying to gh-pages from @ adinagherghisan/goit-js-hw-09@77334673b5d191e69561b32c216c0417baa40373 🚀

--- 01-color-switcher.3206452a.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;let o=null;t.addEventListener("click",(function(){t.disabled=!0,o=setInterval((function(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){e.disabled=!1,clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.3206452a.js.map
